b5c3075fbaad6909d76fefc48e651ca2106d1ca5
Nathan Woodburn <github@nathan.woodburn.au>
2025-01-31 20:17:52 +1100

    fix: Use double quotes for rendering DS record

--- render.py
+++ render.py
@@ -105,7 +105,7 @@ def dns(data, edit=False):
 
 
         elif entry['type'] == 'DS':
-            ds = f'{entry['keyTag']} {entry['algorithm']} {entry['digestType']} {entry['digest']}'
+            ds = f"{entry['keyTag']} {entry['algorithm']} {entry['digestType']} {entry['digest']}"
             html_output += f"<td>{ds}</td>\n"
 
         else:
